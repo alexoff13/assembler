6ee1c76c27a545157dce7d3c9fa607ea6743f4bc
alexoff13 <davydovaleksey932@gmail.com>
2020-10-13 10:15:06 +1000

    build solution

---
+++ AssemblerLabs1/AssemblerLabs1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <conio.h>
+
+
+
+static void Calculate() {
+    int buf;
+    _asm
+    {
+        mov eax, 16;
+        mov edx, 0;
+        mov ebx, 3;
+        div ebx;
+        mov buf, eax;
+        mov eax, 8;
+        mov ebx, 2;
+        mul ebx;
+        mov  edx, buf;
+        sub eax, edx;
+        mov buf, eax;
+        mov eax, 3;
+        mov ebx, 5;
+        mul ebx;
+        mov ebx, buf;
+        sub eax, ebx;
+        mov buf, eax;
+        mov eax, 15;
+        mov ebx, 6;
+        div ebx;
+        mov ebx, eax;
+        mov eax, buf;
+        mov edx, 0;
+        div ebx;
+        mov buf, eax;
+    }
+
+    std::cout << buf << std::endl;
+
+}
+
+static void Inverse() {
+
+    _asm {
+        mov ax, 1110111101010101b;
+        mov bh, ah;
+        mov ah, al;
+        mov al, bh;
+        shr ax, 5;
+        not ax;
+    }
+}
+
+
+
+int main()
+{
+    //Calculate();
+    Inverse();
+    return 0;
+}
+
---
+++ AssemblerLabs2/AssemblerLabs2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+static void Calculate() {
+    short x,a,res,y1, y2;
+    std::cout << "Please, enter a:";
+    std::cin >> a;
+    std::cout << "Please, enter x:";
+    std::cin >> x;
+    _asm {
+
+        cmp x, ax;
+        JA higher;
+    lower:
+        mov y1, 5;
+    higher:
+        mov ax, x;
+        mov dx, a;
+        sub ax, dx;
+        mov y1, ax;
+        mov ax, a;
+        cmp ax, x;
+        JA higher2;
+    lower2:
+        mov ax, a;
+        mov y2, ax;
+    higher2:
+        mov ax, x;
+        mov bx, a;
+        mul bx;
+        mov y2, ax;
+        mov ax, y1;
+        mov dx, y2;
+        sub ax, dx;
+        mov res, ax;
+
+    }
+    std::cout << res;
+}
+
+int main()
+{
+    Calculate();
+}
+
